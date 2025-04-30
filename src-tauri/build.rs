6f4fc1bb28eba62da6ea47c9edb872bfb1857ce1
u-keun song <u-keunsong@u-keunui-MacBookAir-2.local>
2025-04-30 12:30:39 +0900

    Chore: revise build.rs

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,17 @@
+use std::{ env, fs, path::PathBuf, };
+
 fn main() {
+    let out_dir = PathBuf::from(env::var_os("OUT_DIR").unwrap());
+
+    let manifest_dir = PathBuf::from(env::var_os("CARGO_MANIFEST_DIR").unwrap());
+
+    let src_conf = manifest_dir.join("tauri.conf.json");
+    let dst_conf = out_dir.join("tauri.conf.json");
+    fs::create_dir_all(&out_dir).unwrap();
+    fs::copy(&src_conf, &dst_conf)
+        .expect("failed to copy tauri.conf.json into OUT_DIR");
+
+    env::set_current_dir(&out_dir).unwrap();
+
     tauri_build::build()
 }
