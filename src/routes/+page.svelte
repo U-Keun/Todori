a1c420d497a961733002932518f1d739bef2cd65
u-keun song <u-keunsong@u-keunui-MacBookAir-2.local>
2025-05-12 13:39:38 +0900

    refactor: rename TodoList.svelte -> TodoTree.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-    import TodoList from '$lib/components/TodoList.svelte';   
+    import TodoTree from '$lib/components/TodoTree.svelte';   
 </script>
 
 <main class="min-h-screen flex flex-col items-center
              bg-gradient-to-b py-10">
     <div class="w-full max-w-lg">
-        <TodoList />
+        <TodoTree />
     </div>
 </main>
